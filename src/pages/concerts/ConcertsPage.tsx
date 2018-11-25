c5323d3e0f0209a0d59a96a06cd3dca6526cec82
danben4 <danielbenediktsson4@gmail.com>
2018-11-25 13:00:19 +0000

    displaying loading indicator

--- src/pages/concerts/ConcertsPage.tsx
+++ src/pages/concerts/ConcertsPage.tsx
@@ -3,37 +3,42 @@ import { concertAPI } from '../../api/concert';
 import { IConcertEntity } from '../../models/concertEntity';
 
 interface IState {
+  loading: boolean,
   concerts: IConcertEntity[];
 }
 
 export class ConcertsPage extends React.Component<{}, IState> {
   constructor(props: {}) {
     super(props);
-    this.state = { concerts: [] };
+    this.state = { loading: true, concerts: [] };
   }
 
   public componentDidMount() {
     concertAPI.fetchConcertsAsync()
       .then((concerts) => {
-        this.setState({ concerts });
+        this.setState({ loading: false, concerts });
       });
   }
 
   public render() {
-    const { concerts } = this.state;
+    const { loading, concerts } = this.state;
     return (
       <div>
         <h1>
           Concerts
         </h1>
-        <div>
-          {
-            concerts.map(
-              (concert: IConcertEntity, index: number) =>
-                <div key={"concert" + index}>{concert.name}</div>
-            )
-          }
-        </div>
+        {
+          loading ?
+            <div>Loading..</div> :
+            <div>
+              {
+                concerts.map(
+                  (concert: IConcertEntity, index: number) =>
+                    <div key={"concert" + index}>{concert.name}</div>
+                )
+              }
+            </div>
+        }
       </div>
     );
   }
